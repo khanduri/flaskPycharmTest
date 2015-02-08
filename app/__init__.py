102aa4de7305e6f4f67c34fac4306de171a87753
Prashant Khanduri <pkhanduri@jawbone.com>
2015-02-08 12:08:27 -0800

    Sample sitemap implementation

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 
 app = Flask(__name__)
@@ -6,6 +6,22 @@ app = Flask(__name__)
 
 from app.compute import api
 from app.compute import routes
+
+
+def has_no_empty_params(rule):
+    defaults = rule.defaults if rule.defaults is not None else ()
+    arguments = rule.arguments if rule.arguments is not None else ()
+    return len(defaults) >= len(arguments)
+
+
+@app.route("/sitemap")
+def site_map():
+    links = []
+    for rule in app.url_map.iter_rules():
+        if "GET" in rule.methods and has_no_empty_params(rule):
+            url = url_for(rule.endpoint)
+            links.append((url, rule.endpoint))
+    return "</br >".join([path for path, name in links])
 
 
 __all__ = [
